69a719499aa4fb881672208fad071a21bcfced36
Guilherme Travinski Ferreira <guitrafer@gmail.com>
2022-01-16 21:12:11 +0000

    Send console log

---
+++ src/CreateCourseService.ts
@@ -0,0 +1,10 @@
+
+
+class CreateCourseService {
+
+    execute(name: string, duration: number, educator: string) {
+        console.log(name, duration, educator);
+    }
+}
+
+export default new CreateCourseService();
